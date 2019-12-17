c6cef6e3231a530b1815cfecd15bd486694273f1
dongjin.ahn <adoji92@gmail.com>
2019-12-17 12:49:28 +0900

    Copy generator code as same as the book

---
+++ javascript/generator_main.ts
@@ -0,0 +1,43 @@
+function ant(n: number) {
+  let s = gen([1]);
+  for (let i = 1; i < n; i += 1) {
+    s = next(s);
+  }
+  return s;
+}
+
+function printAnt(antGen: Generator) {
+  for (const num of antGen) {
+    process.stdout.write((num as number).toString());
+  }
+  console.log(); // NOTE: for new line
+}
+
+function* gen(x: any) {
+  yield* x;
+}
+
+function* next(g: Generator) {
+  let prev = g.next().value;
+  let count = 1;
+  for (let value of g) {
+    if (value === prev) {
+      count += 1;
+    } else {
+      yield* [count, prev];
+      prev = value;
+      count = 1;
+    }
+  }
+  yield* [count, prev];
+}
+
+function main() {
+  for (let i = 1; i <= 10; i += 1) {
+    const a = ant(i);
+    process.stdout.write(`i ${i}, `);
+    printAnt(a);
+  }
+}
+
+main();
